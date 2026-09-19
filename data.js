c0eac137f308f3e4bcb5c439432f157e1576b4a3
askolaninasution-cyber <askolani.nasution@gmail.com>
2026-09-19 22:31:04 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const dbaseData = [
+  { col1: "Judul/Item Pertama", col2: "Detail isi dari item pertama..." },
+  { col1: "Judul/Item Kedua", col2: "Detail isi dari item kedua..." },
+  { col1: "Judul/Item Ketiga", col2: "Detail isi dari item ketiga..." }
+  { col1: "Judul/Item Pertama", col2: "Detail isi dari item pertama..." },
+  { col1: "Judul/Item Kedua", col2: "Detail isi dari item kedua..." },
+  { col1: "Judul/Item Ketiga", col2: "Detail isi dari item ketiga..." }
+];
